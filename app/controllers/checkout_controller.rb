a6d0ac6bc8fb6d766af1bec61817fea8c6760b34
TallAsian <elijah.rohi@gmail.com>
2023-12-14 20:47:19 -0600

    finished chckout

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -6,34 +6,31 @@ class CheckoutController < ApplicationController
     
         # Province filter logic
         # Find the province based on the entered name
-        @province = Provinces.find_by(prov_name: params[:province_name])
+        if params[:province_name].present?
+            @province = Provinces.find_by(prov_name: params[:province_name])
+        end
+        if @province.present?
+            # Find the province based on the entered name
+              # Create a new customer with a random hash and associate with the province
+              customer = Customer.create(
+              cust_name: SecureRandom.hex(8),
+              provinces_id: @province.id # Use @province.id directly
+              )
+          
+              # Loop through cart items to create orders for each item
+              @cart_items.each do |item|
+              order = Order.create(
+                  customers_id: customer.id.to_i,
+                  products_id: item.id.to_i,
+                  quantity: session[:quantity][item.id.to_s].to_i
+              )
+              end
         
+            redirect_to success_path_url # Redirect to a success page or another relevant action
+        end
         
 
         # Calculate total cost with taxes
     end
-    def confirm_checkout
-        if params[:province_name].present?
-            # Create a new customer with a random hash and associate with the province
-            customer = Customer.find_or_create_by(
-              cust_name: SecureRandom.hex(8),
-              provinces_id: params[:province_name].id
-            )
-        
-            # Loop through cart items to create orders for each item
-            @cart_items.each do |item|
-                order = Order.create(
-                  customers_id: customer.id.to_i,
-                  products_id: item.id.to_i,
-                  quantity: session[:quantity][item.id.to_s].to_i
-                )
-            end
-            redirect_to success_path # Redirect to a success page or another relevant action
-          else
-            # redirect_to checkout_index_path # Redirect back to the checkout page with an error message
-          end
-      end
-    def success
-        
-    end
+      
 end
